a3a3def67b3d0f22142100dc8edd1a24bc2a3e8f
Shai_y <shaiyardeni003@gmail.com>
2023-10-18 17:28:11 +0300

    renamed main

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.post('/service', async (req, res) => {
     const {client,product} = req.body.request //Destructure user input from field group 'request'
     req.session.productName = product //Store product-name for upcoming use in 'thanks' route
     let insert = 'INSERT INTO requests SET ?'
-    let obj = {client:client, product:product}
+    let obj = {client:client, product:product};
     try {
-        let result = await pool.query(insert, obj)
+        let result = await pool.query(insert, obj);
     }catch(e) {
-        console.log(`Error #${e.errno} - ${e.sqlMessage}`)
+        console.log(`Error #${e.errno} - ${e.sqlMessage}`);
         return res.redirect('/error');
     }
     res.redirect('/thanks');
@@ -46,13 +46,13 @@ app.get('/thanks', async (req, res) => {
         let num = results[0].count //Destructure the count object from first array
         res.render('thanks',{num});
     }catch(e) {
-        console.log(`Error #${e.errno} - ${e.sqlMessage}`)
+        console.log(`Error #${e.errno} - ${e.sqlMessage}`);
         return res.redirect('/error');
     }
 });
 
 app.get('/error', (req,res) => {
-    res.render('error')
+    res.render('error');
 });
 
 app.listen(3000, () => {
